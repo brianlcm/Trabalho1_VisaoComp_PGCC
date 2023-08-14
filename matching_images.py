a90bbd6dd2cab5cf2e7b24a2c3f533db8fed46cf
brianlcm <brianluiscoimbra@gmail.com>
2023-08-13 23:34:43 -0300

    Inicia refatoracao e faz teste de panorama

--- matching_images.py
+++ matching_images.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 
-def pointsMap(img1, img2):
+def run_matching_images(img1, img2):
 
+    # Converte as imagens para escala de cinza
     img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
@@ -14,9 +15,9 @@ def pointsMap(img1, img2):
     img2_keypoints, img2_descriptors = sift.detectAndCompute(img2_gray, None)
 
     # Salva as imagens com os pontos de interesse
-    cv2.imwrite('images\\keypoints-1.png',
+    cv2.imwrite('results\\keypoints-1.png',
                 cv2.drawKeypoints(img1, img1_keypoints, img1))
-    cv2.imwrite('images\\keypoints-2.png',
+    cv2.imwrite('results\\keypoints-2.png',
                 cv2.drawKeypoints(img2, img2_keypoints, img2))
     
     # Chama a funcao BFMatcher do OpenCV para forcar a matching entre as duas imagens 
@@ -24,7 +25,7 @@ def pointsMap(img1, img2):
     matches = matcher.match(img1_descriptors,img2_descriptors)
 
     # Cria array com o mapeamento dos pontos de correspondencia encontrados entre as duas imagens
-    point_map = np.array([
+    pointsMap = np.array([
             [img1_keypoints[match.queryIdx].pt[0],
             img1_keypoints[match.queryIdx].pt[1],
             img2_keypoints[match.trainIdx].pt[0],
@@ -33,6 +34,6 @@ def pointsMap(img1, img2):
 
     # Salva a imagens com todos os pontos de correspondencia
     matched_image = cv2.drawMatches(img1, img1_keypoints, img2, img2_keypoints, matches, None, flags=2)
-    cv2.imwrite('images\\matches_BFMatcher.png', matched_image)
+    cv2.imwrite('results\\matches_BFMatcher.png', matched_image)
 
-    return point_map
+    return pointsMap
