b48dbc66817e8c12171f1c40114f10a3b380164e
brianlcm <brianluiscoimbra@gmail.com>
2023-08-14 02:55:05 -0300

    Adiciona comentarios em ingles no codigo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Importando bibliotecas e arquivos necessarios
+# Importing required libraries and files
 import cv2
 import matching_images
 import ransac
@@ -7,27 +7,29 @@ import generate_panorama
 
 def main():
     
-    # Define o par de imagens que sera usado para criar a imagem panoramica
+    # Defines the image pair that will be used to create the panoramic image
     pair_imgs = 'A' # A, B, C, D
 
-    # Faz a leitura do par de imagens a partir da pasta 'images'
+    # Reads the image pair from the 'input_images' folder
     imgInput1 = cv2.imread('input_images\\pair_images_{}\\input1.jpg'.format(pair_imgs))
     imgInput2 = cv2.imread('input_images\\pair_images_{}\\input2.jpg'.format(pair_imgs))
 
-    # Chama a funcao para fazer a correspondencia entre as imagens e retornar o mapa de pontos
+    # Invokes the function to match the images and return the point map.
     pointsMap = matching_images.run_matching_images(imgInput1, imgInput2, pair_imgs)
     
-    # Chama a funcao do para computar a homografia a partir do RANSAC
+    # Invokes the do function to compute the homography from RANSAC
     homography, inliers = ransac.run_RANSAC(pointsMap)
 
-    # Desenha os inliers nas duas imagens apos o uso do RANSAC
+    # Draw the inliers on the two images after using RANSAC
     util.draw_inlines(inliers, imgInput1, imgInput2, pair_imgs)
 
-    # Estima H utilizando os inliers da melhor solucao candidata
+    # Estimates H using the inliers of the best candidate solution
     homography = ransac.HomographyGenerator(inliers)
     
+    # Invokes the function to generate the panoramic image
     panoramicImage = generate_panorama.fit_transform_homography(homography, imgInput1, imgInput2)
 
+    # Saves final panoramic image
     cv2.imwrite('results\\pair_images_{}\\panoramic_image.png'.format(pair_imgs), panoramicImage)
     
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -4,20 +4,20 @@ import numpy as np
 # Function to draw lines to indicate correspondence
 def draw_inlines(inliers, img_1, img_2, pair_imgs):
 
-        # Define alguns parametros para desenhar os inliers
-        lineColor = [0, 255, 0] # A cor da linha foi definida como verde
-        ptColor = [0, 0, 0] # A cor do ponto foi definida como preto
-        lineThickness = 2
-        radius = 5
-        circThickness = 2
+        # Define some parameters to draw the inliers
+        lineColor = [0, 255, 0] # The color of the line has been set to green
+        ptColor = [0, 0, 0] # The color of the point has been set to black
+        lineThickness = 2 # Line thickness
+        radius = 5 # radius of circles
+        circThickness = 2 # Thickness of circles
 
         # Shape function returns a tuple of the number of rows, columns, and channels
         h, w, _ = img_1.shape 
 
-        # Utiliza funcao hstack do numpy para empilhar os arrays das duas imagens
+        # Use numpy's hstack function to stack the arrays of the two images.
         imgStack = np.hstack((img_1, img_2))
 
-        # Loop para passar percorrer os inliers e desenhar os circulos e linhas nas duas imagens
+        # Loop to go through the inliers and draw the circles and lines in the two images.
         for x1, y1, x2, y2 in inliers:
             x1_d = int(round(x1))
             y1_d = int(round(y1))
@@ -25,14 +25,16 @@ def draw_inlines(inliers, img_1, img_2, pair_imgs):
             x2_d = int(round(x2) + w)
             y2_d = int(round(y2))
 
+            # Draw circles on images
             cv2.circle(imgStack, (x1_d, y1_d), radius=radius, color=ptColor,
                        thickness=circThickness, lineType=cv2.LINE_AA)
 
             cv2.circle(imgStack, (x2_d, y2_d), radius=radius, color=ptColor,
                        thickness=circThickness, lineType=cv2.LINE_AA)
 
+            # Draw lines on images
             cv2.line(imgStack, (x1_d, y1_d), (x2_d, y2_d), color=lineColor,
                      thickness=lineThickness)
 
-        # Salva a imagem com as marcacoes
+        # Save the image with the markings
         cv2.imwrite('results\\pair_images_{}\\matches_RANSAC.png'.format(pair_imgs), imgStack)
